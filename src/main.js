b85a12c3b5c3d0b4ad1518db7151d14e5fc6ce68
WannCatchMyDream <catchmydream@qq.com>
2018-08-18 18:02:29 +0800

    chores:整理文件

---
+++ src/main.js
@@ -0,0 +1,28 @@
+//引入vue
+import Vue from 'vue';
+
+//引入App.vue
+import App from './App.vue';
+import axios from 'axios';
+
+//引入路由
+import router from './router/index.js'
+
+//挂载到vue原型树上
+Vue.prototype.$axios = axios;
+
+// axios.defaults.baseURL = "https://sp0.baidu.com";
+
+Vue.prototype.HOST = '/api'; //将conif中index.js里面的api映射拿过来
+
+//new vue
+new Vue(
+  {
+  //目的地
+  el: '#app',
+  router,
+  //渲染内容
+  render: c=>c(App)
+}
+)
+
